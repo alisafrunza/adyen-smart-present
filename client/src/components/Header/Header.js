cc3d2a34d69edd3dadbbf3b95902acf6f1be0c9d
gitstephanie <s.dekjong@mail.com>
2019-02-09 16:08:25 +0100

    header styling

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,15 +1,13 @@
 import React, { Fragment } from 'react';
 import './style.css';
-import './'
 
 const Header = () => (
     <Fragment>
         <header className="header">
-            <img src="./logo.png" alt="logo" className="logo"></img>
-            <div className="user-nav__user">
-                <img src="./maggie.png" alt="User" className="user-nav__user-photo"></img>
-                <span className="user-nav__user-name">Maggy</span>
-            </div>
+            <img src={require("./logo.png")} alt="logo" className="logo"></img>
+            <span className="money_donated">Donated so far</span>
+            <span className="user-nav__user-name">Maggie's wishlist</span>
+            <img src={require("./maggie.png")} alt="User" className="user-photo"></img>
         </header>
     </Fragment>
 );
--- client/src/components/WishList/WishList.js
+++ client/src/components/WishList/WishList.js
@@ -1,2 +1,16 @@
 import react from 'react';
 import List from '@material-ui/core/List';
+
+const WishList = () => (
+    <Fragment>
+        <header className="header">
+            <img src={require("./logo.png")} alt="logo" className="logo"></img>
+            <div className="user-nav__user">
+                <img src={require("./maggie.png")} alt="User" className="user-nav__user-photo"></img>
+                <span className="user-nav__user-name">Maggie</span>
+            </div>
+        </header>
+    </Fragment>
+);
+
+export default WishList;
