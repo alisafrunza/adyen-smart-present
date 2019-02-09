fa5a7610b7751de5e4521eb5a02be7a6d5b9ad47
alisafrunza <alisafrunza@gmail.com>
2019-02-09 14:12:46 +0100

    Register receiver and create wallet

---
+++ server/db/migrate/20190209130902_wallet.rb
@@ -0,0 +1,10 @@
+class Wallet < ActiveRecord::Migration[5.2]
+  def change
+    create_table :wallets do |t|
+      t.decimal :amount
+      t.belongs_to :receiver
+
+      t.timestamps
+    end
+  end
+end
